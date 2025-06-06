0082d75b37d64ded3ebdb38d89feeec982a28fa7
kang <hust.wangkang@gmail.com>
2025-06-06 23:14:00 +0800

    完成一个函数的对接，普通的callback

--- src/TestService.cpp
+++ src/TestService.cpp
@@ -4,7 +4,9 @@
                                                              const ::TimeConsumingRequest* request,
                                                              ::TimeConsumingResponse* response) {
   response->set_status(4);
+  std::this_thread::sleep_for(std::chrono::seconds(2));
   auto* reactor = context->DefaultReactor();
+  std::cout << "【" << std::this_thread::get_id() << "】" << "time consuming method has been called" << std::endl;
   reactor->Finish(grpc::Status::OK);
   return reactor;
 }
